f67d25410ced4154f89246c71ff415cec8ce5911
SBinakin <sundaybinakin@gmail.com>
2025-03-05 09:51:39 +0000

    smooth transiione made

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
-import Footer from "../components/Footer";
+import Footer from "../components/Footer"
 
 const RootLayout = () => {
   return (
-    <div>
+    <>
       <Navbar />
       <Outlet />
-      <Footer />
-    </div>
+      <Footer/>
+    </>
   );
 };
 
